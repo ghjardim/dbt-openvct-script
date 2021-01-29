88f65bb863c61fbeb7c0863b97047a441a9e6a00
ghjardim <ghjardim@gmail.com>
2021-01-29 16:43:28 -0300

    Start main batch file

---
+++ run.bat
@@ -0,0 +1,5 @@
+BreastPhantomGenerator.exe -h
+SliceVctPhantom.exe -h
+BreastPhantomDeformer.exe -xml_input <xml_input_file>
+BreastPhantomInserter.exe -xml_input <xml_input_file>
+XPLProjectionSIM_GPU.exe -xml_input <xml_input_file>
